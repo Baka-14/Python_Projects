598e13f0023795ef59bf28513c4637db7e6d098f
srikar <ventrapragadasaisrikar123@gmail.com>
2022-06-25 23:47:40 +0530

    adding new file

---
+++ nightsky.py
@@ -0,0 +1,14 @@
+from turtle import *
+import random
+import turtle
+
+speed(0)
+turtle.screensize(canvwidth=500, canvheight=300, bg="white")
+penup()
+goto(-350,-250)
+pendown()
+
+
+forward(10)
+
+mainloop()
